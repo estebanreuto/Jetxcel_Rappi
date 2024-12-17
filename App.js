c7327b3238765d51d1ff5d132b251ec785ed1d0b
estebanreuto <142682572+estebanreuto@users.noreply.github.com>
2024-12-17 16:50:18 -0500

    Prueba de renderizar

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Screen2 from './screens/Tutorial/screen2';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Screen2/>
   );
 }
 
